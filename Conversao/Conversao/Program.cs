57ff179236ec1e0a051b91fdceafee13ecbc56ae
DaniloS135 <danilosouza.s135@gmail.com>
2024-08-28 07:56:42 -0300

    Método Parse

--- Conversao/Conversao/Program.cs
+++ Conversao/Conversao/Program.cs
@@ -33,24 +33,44 @@ namespace Conversao
             //Console.ReadKey();
             #endregion
 
-            ushort num1 = 100;
-            byte num2 = (byte)num1; //se não colocarmos o tipo de valor entre () vai dar erro no projeto pelo motivo de estarmos tentando puxar uma váriavel maior para uma menor. Colocando o tipo de dados entre (), informamos que sabemos dos riscos de perda de dado e que pode puxar os dados da outra variavel.
+            #region Conversão Explícita
+            //ushort num1 = 100;
+            //byte num2 = (byte)num1; //se não colocarmos o tipo de valor entre () vai dar erro no projeto pelo motivo de estarmos tentando puxar uma váriavel maior para uma menor. Colocando o tipo de dados entre (), informamos que sabemos dos riscos de perda de dado e que pode puxar os dados da outra variavel.
 
-            Console.WriteLine(num2);
+            //Console.WriteLine(num2);
+            //Console.ReadKey();
+
+            //float num3 = 2500f;
+            //int num4 = (int)num3;
+
+            //float num5 = 2500.786f;
+            //int num6 = (int) 3500.45f;  //mostra que não precisa colocar o "float", pode colocar o dado/número direto e apenas informar a variavel que deseja "limitar"/abranger 
+
+            //Console.WriteLine(num4);
+            //Console.ReadKey();
+
+            //char letra = (char)97;
+            //Console.WriteLine(letra);
+            //Console.ReadKey();
+            #endregion
+
+            string txtNumero = "1985";
+
+            int numero = int.Parse (txtNumero);
+
+            byte num1 = byte.Parse("155");
+
+            double num2 = double.Parse ("125632,57");
+
+            float num3 = float.Parse("457,75");
+
+            //todos os tipos númericos possuem o método Parse, onde temos que passar por parametro um valor string que represente um valor númerico. A string pode estar em formato de variavel ou na forma literal.
+            //lembrando que as casas decimais devem ser separadas por virgula e não por pontos e o float não precisa da letra F
+
+
+            Console.WriteLine(numero);
             Console.ReadKey();
 
-            float num3 = 2500f;
-            int num4 = (int)num3;
-
-            float num5 = 2500.786f;
-            int num6 = (int) 3500.45f;  //mostra que não precisa colocar o "float", pode colocar o dado/número direto e apenas informar a variavel que deseja "limitar"/abranger 
-
-            Console.WriteLine(num4);
-            Console.ReadKey();
-
-            char letra = (char)97;
-            Console.WriteLine(letra);
-            Console.ReadKey();
 
 
 
