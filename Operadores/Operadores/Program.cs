caa30db2c7eda3f6c1a10d196079188bba250b04
DaniloS135 <danilosouza.s135@gmail.com>
2024-08-30 08:49:37 -0300

    Operadores Matemáticos

---
+++ Operadores/Operadores/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1 = 10;
+            int num2 = 2;
+
+            int resultado = num1 + num2;
+
+            Console.WriteLine(resultado); //ou podemos usar sem a variavel, ex: Console.WriteLine (num1 + num2);
+            Console.WriteLine(num1 - num2);
+            Console.WriteLine(num1 * num2);
+            Console.WriteLine(num1 / num2);
+            Console.WriteLine(num1 % num2);
+            Console.ReadKey();
+
+
+        }
+    }
+}
