aaf6e5c6d35e593b6b60acd8e44b3f35c298cf3b
DaniloS135 <danilosouza.s135@gmail.com>
2024-08-30 10:03:23 -0300

    Prioridade de Sinais

--- Operadores/Operadores/Program.cs
+++ Operadores/Operadores/Program.cs
@@ -10,19 +10,29 @@ namespace Operadores
     {
         static void Main(string[] args)
         {
+            #region Operadores Matemáticos
+            //int num1 = 10;
+            //int num2 = 2;
 
-            int num1 = 10;
-            int num2 = 2;
+            //int resultado = num1 + num2;
 
-            int resultado = num1 + num2;
+            //Console.WriteLine(resultado); //ou podemos usar sem a variavel, ex: Console.WriteLine (num1 + num2);
+            //Console.WriteLine(num1 - num2);
+            //Console.WriteLine(num1 * num2);
+            //Console.WriteLine(num1 / num2);
+            //Console.WriteLine(num1 % num2);
+            //Console.ReadKey();
+            #endregion
 
-            Console.WriteLine(resultado); //ou podemos usar sem a variavel, ex: Console.WriteLine (num1 + num2);
-            Console.WriteLine(num1 - num2);
-            Console.WriteLine(num1 * num2);
-            Console.WriteLine(num1 / num2);
-            Console.WriteLine(num1 % num2);
+            int num1 = 100;
+            int num2 = 10;
+            int num3 = 5;
+
+            int res = (num1 + num2) * num3;
+
+            Console.WriteLine(res);
             Console.ReadKey();
-
+            //ordem de prioridade = 1° (), 2° * e /, 3° + e -. Se tiverem o mesmo nivel de prioridade, vão fazer da esquerda para direita.
 
         }
     }
